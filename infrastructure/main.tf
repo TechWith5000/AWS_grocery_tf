4b96e845232fe9c289f0acce33138cd8bbea0d54
TechWith5000 <janphilippgutt5000@gmail.com>
2025-04-22 18:14:57 +0200

    added random_id for randomized bucket suffix

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -3,6 +3,14 @@ provider "aws" {
   region = var.aws_region
 }
 
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+    }
+  }
+}
+
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
   enable_dns_hostnames = true
--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -1,5 +1,10 @@
+resource "random_id" "bucket_suffix" {
+  byte_length = 4
+}
+
 resource "aws_s3_bucket" "avatars" {
-  bucket = "grocerymate-avatars"
+  bucket = "grocerymate-avatars-${random_id.bucket_suffix.hex}"
+  force_destroy = true
 
   tags = {
     Name        = "grocerymate-avatars"
