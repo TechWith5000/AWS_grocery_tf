4230a1c51db83ab5b43fb6a3a2789e277232fde0
TechWith5000 <janphilippgutt5000@gmail.com>
2025-04-22 07:47:59 +0200

    updated postgresql version in main.tf to match available versions in eu region

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -144,7 +144,7 @@ resource "aws_security_group" "db_sg" {
 resource "aws_db_instance" "postgres" {
   identifier              = "my-postgres-db"
   engine                  = "postgres"
-  engine_version          = "15.4"
+  engine_version          = "15.12"
   instance_class          = "db.t3.micro"
   allocated_storage       = 20
   storage_type            = "gp2"
